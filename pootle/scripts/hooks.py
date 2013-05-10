2da3e3e06c05e5c3aff4cd1c15efcf1e05b4ba6b
Alexander Dupuy <alex.dupuy@mac.com>
2013-05-10 02:09:09 -0400

    Clean up exception handling for hooks

--- pootle/scripts/hooks.py
+++ pootle/scripts/hooks.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009,2013-2013 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+""" Dynamic loading of hooks for update and commit """
+
 
 import logging
 
@@ -30,15 +51,15 @@ def hook(project, hooktype, file, *args, **kwargs):
         if (hasattr(activehook, hooktype) and
                 callable(getattr(activehook, hooktype))):
             logger.debug("Executing hook %s for project %s on file %s",
-                         hooktype, project, file)
-            return getattr(activehook, hooktype)(file, *args, **kwargs)
-        else:
-            logger.debug("Imported %s, but it is not a suitable %s hook",
-                         activehook.__file__, hooktype)
-            raise ImportError("Imported %s, but it is not a suitable %s hook" %
-                              (activehook.__file__, hooktype))
+                         hooktype, project, path)
+            return getattr(activehook, hooktype)(path, *args, **kwargs)
     except ImportError, e:
         raise ImportError(e)
-    except Exception, e:
-        logger.error("Exception in project (%s) hook (%s) for file (%s): %s" %
-                     (project, hooktype, file, e))
+    except StandardError:
+        logger.exception("Exception in project (%s) hook (%s) for file (%s)",
+                         project, hooktype, path)
+    else:
+        logger.debug("Imported %s, but it is not a suitable %s hook",
+                     activehook.__file__, hooktype)
+        raise ImportError("Imported %s, but it is not a suitable %s hook" %
+                          (activehook.__file__, hooktype))
